b6248347887153951615c8f3e989c0f6dad17a12
Josh Chappelow <jdchappelow@gmail.com>
2023-02-20 23:15:04 +0900

    build: docker setup

--- backend/main.py
+++ backend/main.py
@@ -17,6 +17,19 @@ DB: List[Person] = [
 
 ]
 
+##Docker
+
+#Back end
+#run --name backend --rm --network foobar  -p 8000:8000 backend
+
+#Frontend
+#docker run --rm --name frontend --network foobar -p 3000:3000 frontend
+
+
+
+# OLD
+# Info comes from container not local machine if you use command below
+# docker run --name backend --rm -p 8000:8000 backend
 
 # Remember to run uvicorn main:app --reload for serv and npm start for frontend
 
